1cadd2b9cb8b3dc579f77db9e83af45332991b27
Sang Yeop <piegeek@Sangs-MBP.com>
2026-02-28 16:46:11 +0900

    Working on greedy problems

--- Practice/cses/longestcommonsubsequence.py
+++ Practice/cses/longestcommonsubsequence.py
@@ -12,8 +12,11 @@ def solve_longestcommonsubsequence(n, m, a, b):
     solution = []
     reconstruct(0, 0, n, m, a, b, cache, solution)
 
+    solutions = reconstruct_all(0, 0, n, m, a, b, cache)
+
     print(ret)
-    print(solution)
+    # print(solution)
+    print(solutions)
     return ret
 
 def dp(i, j, n, m, a, b, cache):
@@ -37,12 +40,36 @@ def reconstruct(i, j, n, m, a, b, cache, solution):
     if i == n or j == m:
         return
 
+    current_val = dp(i, j, n, m, a, b, cache)
+
     for idx_a in range(i, n):
         for idx_b in range(j, m):
             if a[idx_a] == b[idx_b]:
-                if dp(i, j, n, m, a, b, cache) == 1 + dp(idx_a + 1, idx_b + 1, n, m, a, b, cache):
+                next_val = dp(idx_a + 1, idx_b + 1, n, m, a, b, cache)
+                if current_val == 1 + next_val:
                     solution.append(a[idx_a])
                     reconstruct(idx_a + 1, idx_b + 1, n, m, a, b, cache, solution)
+                    return
+
+def reconstruct_all(i, j, n, m, a, b, cache):
+    if i == n or j == m:
+        return [[]]
+
+    current_val = dp(i, j, n, m, a, b, cache)
+    results = []
+
+    for idx_a in range(i, n):
+        for idx_b in range(j, m):
+            if a[idx_a] == b[idx_b]:
+                next_val = dp(idx_a + 1, idx_b + 1, n, m, a, b, cache)
+
+                if current_val == 1 + next_val:
+                    suffixes = reconstruct_all(idx_a + 1, idx_b + 1, n, m, a, b, cache)
+                    for suf in suffixes:
+                        results.append([a[idx_a]] + suf)
+
+    return results
+                    
 
 def parse_input():
     input = sys.stdin.readline
